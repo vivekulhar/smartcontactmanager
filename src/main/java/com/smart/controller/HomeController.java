95c5cef66156e7ae593062e8874e126c2c606084
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-04-25 20:19:44 +0530

    Setting up Boostrap and Templates

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
@@ -11,18 +13,35 @@ import com.smart.entities.User;
 @Controller
 public class HomeController {
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test()
+//	@Autowired
+//	private UserRepository userRepository;
+//	
+//	@GetMapping("/test")
+//	@ResponseBody
+//	public String test()
+//	{
+//		User user = new User();
+//		user.setName("Vivek Kulhar");
+//		user.setEmail("vivek@kulhar.com");
+//		
+//		userRepository.save(user);
+//		return "Working";
+//	}
+	@RequestMapping("/home")
+	public String home(Model m)
 	{
-		User user = new User();
-		user.setName("Vivek Kulhar");
-		user.setEmail("vivek@kulhar.com");
 		
-		userRepository.save(user);
-		return "Working";
+		m.addAttribute("title", "Home - Smart Contact Manager");
+		
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model m)
+	{
+		
+		m.addAttribute("title", "About - Smart Contact Manager");
+		
+		return "about";
 	}
 }
