a9a37127d8f8197b05d42ada6af8ca6613624f7e
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-04-25 19:37:57 +0530

    Creating Entities and Tables One to Many

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test()
+	{
+		User user = new User();
+		user.setName("Vivek Kulhar");
+		user.setEmail("vivek@kulhar.com");
+		
+		userRepository.save(user);
+		return "Working";
+	}
+}
