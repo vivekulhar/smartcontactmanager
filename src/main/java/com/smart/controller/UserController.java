6671fa6642aaf6bf9f7fd09ecb7fc46ac47b1767
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-05-03 00:26:36 +0530

    Uploading Image with Contact Details in very Simple steps

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -59,19 +67,53 @@ public class UserController {
 	
 	//Processing add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal)
+	public String processContact(
+			@ModelAttribute Contact contact, 
+			@RequestParam("profileImage") MultipartFile file,
+			Principal principal)
 	{
-		String name = principal.getName();
-		User user = this.userRepository.getUserByUserName(name);
 		
-		contact.setUser(user);
-		user.getContacts().add(contact);
+		try {
+			String name = principal.getName();
+			User user = this.userRepository.getUserByUserName(name);
+			
+			// processing and uploading file..
+			
+			if(file.isEmpty())
+			{
+				//if the file is empty then try our message
+				System.out.println("File is Empty");
+			}else {
+				//upload the file to folder and update the name to contact
+				
+				contact.setImage(file.getOriginalFilename());
+				
+				File saveFile = new ClassPathResource("static/image").getFile();
+				
+				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator + file.getOriginalFilename());
+				
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				
+				System.out.println("Image is Uploaded");
+				
+				
+			}
+			
+			contact.setUser(user);
+			user.getContacts().add(contact);
+			
+			this.userRepository.save(user);
+			
+			System.out.println("DATA" + contact);
+			
+			System.out.println("Added to database");
+		} catch (Exception e) {
+			
+			System.out.println("ERROR" + e.getMessage());
+			
+			e.printStackTrace();
+		}
 		
-		this.userRepository.save(user);
-		
-		System.out.println("DATA" + contact);
-		
-		System.out.println("Added to dataase");
 		return "normal/add_contact_form";
 	}
 }
