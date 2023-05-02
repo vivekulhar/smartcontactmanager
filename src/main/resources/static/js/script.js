04c9a8f53adf77d36dcbf1b132c74cb74033a098
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-05-02 17:49:09 +0530

    Creating Responsive Sidebar for Smart Contact Manager

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,17 @@
-console.log("this is script file")
+console.log("this is script file");
+
+const toggleSidebar = () => {
+
+    if ($(".sidebar").is(":visible")) {
+    //true
+    //band karna hai
+    $(".sidebar").css("display", "none");
+    $(".content").css("margin-left", "0%");
+  } else {
+    //false
+    //show karna hai
+    $(".sidebar").css("display", "block");
+    $(".content").css("margin-left", "20%");
+  }
+
+};
