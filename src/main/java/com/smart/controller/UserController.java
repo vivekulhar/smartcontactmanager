7cc0735638a8d2e6a8e1043cb37d3d8119e2a562
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-05-04 00:07:27 +0530

    Updating Contact in Spring Boot Project

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -221,4 +221,17 @@ public class UserController {
 		return "redirect:/user/show-contacts/0";
 	}
 	
+	//Open update form handler
+	@PostMapping("/update-contact/{cid}")
+	public String updateForm(@PathVariable("cid") Integer cid, Model m)
+	{
+		m.addAttribute("title", "Update Contact");
+		
+		Contact contact = this.contactRepository.findById(cid).get();
+		
+		m.addAttribute("contact", contact);
+		
+		return "/normal/update_form";
+	}
+	
 }
